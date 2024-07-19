0ad2c8cb3753f5aa04827d7784e3601ecffe2693
simon Lab <simelav24@gmail.com>
2024-07-19 23:45:29 +0300

    full render method

--- Calculations.py
+++ Calculations.py
@@ -1,14 +1,66 @@
 
 import numpy as np
-from cmath import sqrt
-from math import sin, cos, pi
+import cmath
+import math
+from math import sin, cos, pi, atan2, log
+from typing import Tuple
+import pygame
 
-def mandel_color(pos: np.ndarray, color=1, max_iterations: int=100):
+image = pygame.image.load('Assets/image.png')
+
+def smap(value, a, b, c, d):
+	return (value - a) / (b - a) * (d - c) + c
+
+class DrawingMethod:
+    def __init__(self, method_index: int=0):
+        self.functions = [
+            (mandel_color, iteration_to_color_bw),
+            (mandel_color, iteration_to_color_colorful),
+            (mandel_color, iteration_to_color_alternating),
+            (mandel_normal, None),
+            (mandel_image, None),
+        ]
+        self.method_index = method_index
+        self.func: callable = self.functions[self.method_index][0]
+        self.coloring: callable = self.functions[self.method_index][1]
+        self.center_pos: np.ndarray = None
+
+    def set_center_pos(self, pos: np.ndarray) -> None:
+        self.center_pos = pos
+
+    def cycle_method(self):
+        self.method_index = (self.method_index + 1) % len(self.functions)
+        self.func: callable = self.functions[self.method_index][0]
+        self.coloring: callable = self.functions[self.method_index][1]
+
+    def draw(self, pos: np.ndarray, max_iteration: int=100) -> Tuple[int, int, int]:
+        center: np.ndarray = pos
+        if self.center_pos is not None:
+            center = self.center_pos
+        return self.func(pos, center, max_iteration, self.coloring)
+
+def iteration_to_color_colorful(iteration: int, max_iterations: int) -> Tuple[int, int, int]:
+    f = 0.1
+    red = (0.5 * sin(f * iteration) + 0.5) * 255
+    green = (0.5 * sin(f * iteration + 2.094) + 0.5) * 255
+    blue = (0.5 * sin(f * iteration + 4.188) + 0.5) * 255
+    return (red, green, blue)
+
+def iteration_to_color_bw(iteration: int, max_iterations: int) -> Tuple[int, int, int]:
+    if iteration == max_iterations:
+        return (0,0,0)
+    else:
+        return (255,255,255)
+
+def iteration_to_color_alternating(iteration: int, max_iterations: int) -> Tuple[int, int, int]:
+    return (255,255,255) if iteration % 2 == 0 else (0,0,0)
+
+def mandel_color(pos: np.ndarray, pos_center: np.ndarray, max_iterations: int=100, coloring: callable=iteration_to_color_bw) -> Tuple[int, int, int]:
     a = pos[0]
     b = pos[1]
     
-    ca = a
-    cb = b
+    ca = pos_center[0]
+    cb = pos_center[1]
     
     iteration = 0
     while iteration < max_iterations:
@@ -26,34 +78,16 @@ def mandel_color(pos: np.ndarray, color=1, max_iterations: int=100):
         
         iteration += 1
     
-    if color == 0: # colorfull, olc
-        f = 0.1
-        red = (0.5 * sin(f * iteration) + 0.5) * 255
-        green = (0.5 * sin(f * iteration + 2.094) + 0.5) * 255
-        blue = (0.5 * sin(f * iteration + 4.188) + 0.5) * 255
-        return (red, green, blue)    
-    
-    elif color == 1: # black&white
-        if iteration == max_iterations:
-            return (0,0,0)
-        else:
-            return (255,255,255)
-    
-    elif color == 2:
-        return (255,255,255) if iteration % 2 == 0 else (0,0,0)
+    return coloring(iteration, max_iterations)
 
-
-def mandel_normal(pos, color, max_iterations=100):
+def mandel_normal(pos: np.ndarray, pos_center: np.ndarray, max_iterations=100, coloring: callable=None) -> Tuple[int, int, int]:
     radius = 100
     h2 = 1.5
     angle = 45+180
     v = cos((angle * 2 * pi)/360) + sin((angle * 2 * pi)/360) * 1j
     
-    a = pos[0]
-    b = pos[1]
-    
-    c: complex = a + b * 1j
-    z: complex = a + b * 1j
+    c: complex = pos_center[0] + pos_center[1] * 1j
+    z: complex = pos[0] + pos[1] * 1j
     dc = 1 + 0j
     der = 1 + 0j
     iteration = 0
@@ -89,3 +123,73 @@ def mandel_normal(pos, color, max_iterations=100):
             t = 0
         bright = t * 255
     return (bright, bright, bright)
+
+def mandel_image(pos: np.ndarray, pos_center: np.ndarray, max_iterations=100, coloring: callable=None) -> Tuple[int, int, int]:
+    move = 0
+    out = 4
+    a = pos[0]
+    b = pos[1]
+    
+    ca = pos_center[0]
+    cb = pos_center[1]
+    
+    iteration = 0
+    while iteration < max_iterations:
+        #
+        # za = a
+        # zb = b
+        #
+    
+        aa = a*a - b*b
+        bb = 2*a*b
+
+        a = aa + ca
+        b = bb + cb
+        
+        # if fabs(aa) + fabs(bb) > 4:# asteroids
+        # if fabs(aa + bb) > 4:# droplets
+        # if a*a + b*b > 4:# circular
+        if math.sqrt(a*a + b*b) > out:# circular
+            break
+        
+        iteration += 1
+        
+    
+    # a*=50
+    # b*=50
+    pos = (image.get_width()//2, image.get_height()//2)
+    
+    # col = ((pos[0] + int(za))% image.get_width(), (pos[1] + int(zb)) % image.get_height())
+    
+    x = (atan2(a, b) * (1/pi) + 1)/2
+    x *= image.get_width()
+    x = int((x + move) % image.get_width())
+
+    
+    mag = math.sqrt(a*a + b*b)
+    if not out < mag < out*out:
+        y = 1
+    else:
+        y = log(mag / out) / log(out)
+    y = smap(y, 0, 1, 0, image.get_height() - 1)
+    
+    y = int(y)
+    
+    """ no log:
+    mag = sqrt(a*a + b*b)
+    if not out <= mag <= out*out:
+        y = 0
+    else:
+        y = (mag - out)/(out*out - out)
+    y *= image.get_height()
+    y = int(y)
+    if y > image.get_height():
+        return image.get_height()
+    """
+    
+    # x /= image.get_width()
+    # x *= 255
+    # x = int((x + move) % 255)
+    # print(x)
+    # return (x, x, x)
+    return image.get_at((x,y))
--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
-from math import fabs, sqrt, cos, sin, pi, floor, ceil, e, atan2, log
-from random import uniform, randint, choice
+
+
+import os
 from datetime import datetime
 import numpy as np
 import pygame
@@ -10,18 +11,31 @@ WIN_WIDTH = 1280
 WIN_HEIGHT = 720
 WIN_RATIO = WIN_HEIGHT / WIN_WIDTH
 
-render_size = (1920, 1080)
+OUTPUT_PATH = 'Output'
+
+render_size = (1920*2, 1080*2)
+
+def save_image(surf: pygame.Surface):
+    if not os.path.isdir(OUTPUT_PATH):
+        os.mkdir(OUTPUT_PATH)
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y %H-%M-%S")
+    path = os.path.join(OUTPUT_PATH, f'{dt_string}.png')
+    print(f'Image saved: {path}')
+    pygame.image.save(surf, path)
 
 class Grid:
     def __init__(self):
-        self.cam: np.ndarray = np.array([0,0])
-        self.scale = 200.0
+        self.cam: np.ndarray = np.array([-200,0])
+        self.scale = 300.0
         self.center: np.ndarray = np.array([WIN_WIDTH / 2, WIN_HEIGHT / 2])
         
-        self.point = None
+        self.drag_point = None
         self.cam_prev = None
         self.mouse_pressed: bool = False
         self.is_changed: bool = False
+
+        self.selected_point: np.ndarray = None
         
     def transform(self, pos: np.ndarray) -> np.ndarray:
         return pos * self.scale + self.center - self.cam
@@ -29,7 +43,7 @@ class Grid:
     def invert(self, pos: np.ndarray) -> np.ndarray:
         return (pos - self.center + self.cam) / self.scale
 
-    def zoom(self, sign):
+    def zoom(self, sign: float):
         origin = self.transform(np.zeros(2))
         mouse = np.array(pygame.mouse.get_pos())
         adjust = mouse - origin
@@ -37,9 +51,16 @@ class Grid:
         self.scale += sign * 0.2 * self.scale
         self.is_changed = True
 
+    def home(self):
+        self.cam: np.ndarray = np.array([-200,0])
+        self.scale = 300.0
+        self.center: np.ndarray = np.array([WIN_WIDTH / 2, WIN_HEIGHT / 2])
+        self.drag_point = None
+        self.cam_prev = None
+
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            self.point = np.array(pygame.mouse.get_pos()) / self.scale
+            self.drag_point = np.array(pygame.mouse.get_pos()) / self.scale
             self.mouse_pressed = True
             self.cam_prev = self.cam.copy()
         if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
@@ -53,8 +74,12 @@ class Grid:
         self.is_changed = False
         if self.mouse_pressed:
             current = np.array(pygame.mouse.get_pos()) / self.scale
-            self.cam = self.cam_prev + (self.point - current) * self.scale
+            self.cam = self.cam_prev + (self.drag_point - current) * self.scale
             self.is_changed = True
+
+    def draw(self, win):
+        if self.selected_point is not None:
+            pygame.draw.circle(win, (255,0,0), self.transform(self.selected_point), 5)
 
 def main():
     pygame.init()
@@ -66,6 +91,7 @@ def main():
     font =  pygame.font.SysFont('Tahoma', 12, True)
     
     grid = Grid()
+    drawing = DrawingMethod(1)
 
     initial_subdivisions = 20
     subdivisions_x = initial_subdivisions
@@ -74,6 +100,7 @@ def main():
     
     STATE_EXPLORE = 0
     STATE_RENDER = 1
+    STATE_RENDER_FULL = 2
     state = STATE_EXPLORE
 
     render_finished: bool = False
@@ -84,13 +111,43 @@ def main():
             grid.handle_event(event)
             if event.type == pygame.QUIT:
                 done = True
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if pygame.key.get_mods() & pygame.KMOD_LCTRL:
+                    win_point = grid.invert(np.array(pygame.mouse.get_pos()))
+                    grid.selected_point = win_point
             if event.type == pygame.KEYDOWN:
+
                 if event.key == pygame.K_r:
+                    # render win
                     render_finished = False
                     if state == STATE_EXPLORE:
                         state = STATE_RENDER
                     else:
                         state = STATE_EXPLORE
+
+                if event.key == pygame.K_j:
+                    # switch julia set
+                    if grid.selected_point is None:
+                        continue
+                    if drawing.center_pos is None:
+                        drawing.set_center_pos(grid.selected_point)
+                    else:
+                        drawing.set_center_pos(None)
+
+                if event.key == pygame.K_m:
+                    # cycle methods
+                    drawing.cycle_method()
+                
+                if event.key == pygame.K_p:
+                    # render full
+                    render_finished = False
+                    state = STATE_RENDER_FULL
+                
+                if event.key == pygame.K_h:
+                    # home button
+                    grid.home()
+
+                
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:
             done = True
@@ -117,26 +174,49 @@ def main():
                     pos_in_win = np.array([x_win + (WIN_WIDTH / subdivisions_x) / 2, y_win + (WIN_HEIGHT / subdivisions_y) / 2])
                     # win.set_at((int(pos_in_win[0]), int(pos_in_win[1])), (255,0,0))
                     pos_in_world = grid.invert(pos_in_win)
-                    color = mandel_normal(pos_in_world, 2)
+                    color = drawing.draw(pos_in_world)
                     mandel_surf.set_at((x, y), color)
             subdivisions_x += 1
             subdivisions_y = int(subdivisions_x * WIN_RATIO)
             
             win.blit(pygame.transform.scale(mandel_surf, (WIN_WIDTH, WIN_HEIGHT)), (0,0))
+            grid.draw(win)
         
-        if state == STATE_RENDER:
+        elif state == STATE_RENDER:
             if not render_finished:
                 for y in range(0, WIN_HEIGHT):
                     for x in range(0, WIN_WIDTH):
                         pos_in_win = np.array((x, y))
                         pos_in_world = grid.invert(pos_in_win)
-                        color = mandel_normal(pos_in_world, 2)
+                        color = drawing.draw(pos_in_world)
                         win.set_at((x, y), color)
                     
                     pygame.draw.line(win, (255,255,0), (0, y + 1), (WIN_WIDTH, y + 1))
                     pygame.display.update()
 
-            render_finished = True
+                render_finished = True
+        
+        elif state == STATE_RENDER_FULL:
+            surf = pygame.Surface(render_size)
+            ratio_x = WIN_WIDTH / render_size[0]
+            ratio_y = WIN_HEIGHT / render_size[1]
+            if not render_finished:
+                for y in range(0, render_size[1]):
+                    for x in range(0, render_size[0]):
+
+                        pos_in_win = np.array((x * ratio_x, y * ratio_y))
+                        pos_in_world = grid.invert(pos_in_win)
+                        color = drawing.draw(pos_in_world)
+                        surf.set_at((x, y), color)
+                    
+                    pygame.draw.line(surf, (255,255,0), (0, y + 1), (render_size[0], y + 1))
+
+                    win.blit(pygame.transform.smoothscale(surf, (WIN_WIDTH, WIN_HEIGHT)), (0,0))
+                    pygame.display.update()
+
+                save_image(surf)
+                render_finished = True
+            
 
         # win.blit(mandel_surf, (0,0))
         pygame.display.update()
